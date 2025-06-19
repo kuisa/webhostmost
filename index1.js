f419641616078bd9ce50b5a97f82dec1377bba70
TownMarshal <51028330+TownMarshal@users.noreply.github.com>
2025-06-19 08:07:32 +0800

    Update index1.js

--- index1.js
+++ index1.js
@@ -64,7 +64,7 @@ const httpServer = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello, World\n');
-  } else if (req.url === `/${SUB_PATH}`) {
+  } else if (req.url === `/${UUID}`) {
     const vlessURL = `vless://${UUID}@www.visa.com.tw:443?encryption=none&security=tls&sni=${DOMAIN}&type=ws&host=${DOMAIN}&path=%2F#${NAME}-${ISP}`;
 
     const base64Content = Buffer.from(vlessURL).toString('base64');
