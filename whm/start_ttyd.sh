7d2795c72e9fa8b9689baabfe78fa426a08c0dd6
TownMarshal <51028330+TownMarshal@users.noreply.github.com>
2025-06-26 15:08:50 +0800

    Update start_ttyd.sh

--- whm/start_ttyd.sh
+++ whm/start_ttyd.sh
@@ -2,9 +2,9 @@
 
 # ============ 配置参数 ============
 PORT=35000
-CMD_PATH="/home/xlswatbz/domains/fin.baozong.dpdns.org/public_html/ttyd"
+CMD_PATH="/home/xlswatbz/domains/fin.baozong.dpdns.org/public_html/ttyd"   # ============ 改为自己路径============
 AUTH="town:ts521"
-LOGFILE="/home/xlswatbz/domains/fin.baozong.dpdns.org/public_html/ttyd.log"
+LOGFILE="/home/xlswatbz/domains/fin.baozong.dpdns.org/public_html/ttyd.log"  # ============ 改为自己路径============
 MAX_SIZE=10485760  # 10MB = 10 * 1024 * 1024 bytes
 
 # ============ 清理超大日志 ============
